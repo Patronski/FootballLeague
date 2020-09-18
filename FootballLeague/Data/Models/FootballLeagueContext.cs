ff35ab4f729bda550fea2aa592c3b3b8df699b2d
Doncho Patronski <donstz@yahoo.com>
2020-09-18 13:37:33 +0300

    Add Home controller and EF

---
+++ FootballLeague/Data/Models/FootballLeagueContext.cs
@@ -0,0 +1,32 @@
+namespace FootballLeague.Data.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class FootballLeagueContext : DbContext
+    {
+        // Your context has been configured to use a 'DbContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'FootballLeague.Data.Models.DbContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'DbContext' 
+        // connection string in the application configuration file.
+        public FootballLeagueContext()
+            : base("name=FootballLeagueContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Match> Matches { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
