f2ad65b194f0adf975aa3353d4d7be5ddc5fd3f2
Doncho Patronski <donstz@yahoo.com>
2020-09-18 13:36:34 +0300

    Add data models

---
+++ FootballLeague/Data/Models/Match.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FootballLeague.Data.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+
+        public Team LeftTeam { get; set; }
+
+        public Team RightTeam { get; set; }
+
+        public Tuple<int, int> Result { get; set; }
+    }
+}
---
+++ FootballLeague/Data/Models/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+
+namespace FootballLeague.Data.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public int Points { get; set; }
+
+        public IEnumerable<Match> Matches { get; set; } = new List<Match>();
+    }
+}
